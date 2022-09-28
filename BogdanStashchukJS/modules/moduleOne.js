4faf9a2a3a434205662b28190f970563c1166646
david <david.lisitsyn.dev@gmail.com>
2022-09-28 09:48:12 +0300

    Модули (export, import)

---
+++ BogdanStashchukJS/modules/moduleOne.js
@@ -0,0 +1,10 @@
+const myName = () => {
+  console.log('Bogdan');
+};
+
+const myDog = () => {
+  console.log('BullDog');
+};
+
+export default myName;
+export { myDog };
---
+++ BogdanStashchukJS/modules/moduleTwo.js
@@ -0,0 +1,4 @@
+import printMyName, { myDog as printMyDogName } from './moduleOne.js';
+
+printMyName();
+printMyDogName();
